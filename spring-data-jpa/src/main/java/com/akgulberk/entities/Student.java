368cbc48645a53dce1253da7eac0954892a9a7b5
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-05 14:12:15 +0300

    Spring Data JPA ile Proje Açılışı

--- spring-data-jpa/src/main/java/com/akgulberk/entities/Student.java
+++ spring-data-jpa/src/main/java/com/akgulberk/entities/Student.java
@@ -1,4 +1,31 @@
 package com.akgulberk.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_of_date")
+    private Date birthDate;
+
+
+
 }
--- spring-data-jpa/src/main/java/com/akgulberk/starter/SpringDataJpaApplication.java
+++ spring-data-jpa/src/main/java/com/akgulberk/starter/SpringDataJpaApplication.java
@@ -2,7 +2,9 @@ package com.akgulberk.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.akgulberk"})
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
