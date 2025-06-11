761f61942b4baf4cd49785bdf25761a412f277b3
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-11 20:19:49 +0300

    @PropertySource ve @Value Kullanımı

---
+++ spring-data-jpa/src/main/java/com/akgulberk/controller/impl/PropertySourceController.java
@@ -0,0 +1,25 @@
+package com.akgulberk.controller.impl;
+
+import com.akgulberk.configuration.DataSource;
+import com.akgulberk.configuration.GlobalProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rest/api/property")
+public class PropertySourceController {
+
+    @Autowired
+    private GlobalProperties globalProperties;
+
+    @GetMapping("/datasource")
+    public DataSource getDataSource(){
+        DataSource dataSource = new DataSource();
+        dataSource.setUrl(globalProperties.getUrl());
+        dataSource.setUsername(globalProperties.getUsername());
+        dataSource.setPassword(globalProperties.getPassword());
+        return dataSource;
+    }
+}
