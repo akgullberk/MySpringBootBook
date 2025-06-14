4ef61cf0450ebbc152dc96d282a9096dbceb7406
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-14 15:22:55 +0300

    JUnit Kullanımı

--- exception-management/src/test/java/com/akgulberk/starter/ExceptionManagementApplicationTests.java
+++ exception-management/src/test/java/com/akgulberk/starter/ExceptionManagementApplicationTests.java
@@ -1,13 +1,24 @@
 package com.akgulberk.starter;
 
+import com.akgulberk.dto.DtoEmployee;
+import com.akgulberk.services.IEmployeeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ExceptionManagementApplication.class})
 class ExceptionManagementApplicationTests {
 
+	@Autowired
+	private IEmployeeService  employeeService;
+
 	@Test
-	void contextLoads() {
+	public void testFindEmployeeById(){
+		DtoEmployee employee = employeeService.findEmployeeById(4L);
+		
+//		if(employee!=null){
+//			System.out.println("İsim: " +employee.getName());
+//		}
 	}
 
 }
